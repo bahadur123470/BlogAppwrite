be5aad64619f8a899bf4310eeab25b8a2bd546fa
Bahadur Ali <122412801+bahadur123470@users.noreply.github.com>
2025-08-15 23:18:18 +0500

    sample repo of blog appwrite app react revision

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container"
+import Logo from "./Logo/Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button/Button";
+import Input from "./Input/Input";
+import PostCard from "./PostCard/PostCard";
+import Select from "./Select/Select";
+import Login from "./Login/Login";
+import Signup from "./Signup/Signup";
+import Protected from "./Layout/AuthLayout";
+import RTE from "./RTE/RTE";
+import PostForm from "./PostForm/PostForm";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    PostCard,
+    Select,
+    Login,
+    Signup,
+    Protected,
+    PostForm,
+    RTE,
+
+
+}
